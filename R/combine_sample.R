a3f7fe0c06fcbbfd9e37514a30950947c1603ce4
LeeHaoyu <simon.behrendt@zu.de>
2017-12-29 22:00:26 +0100

    Bootstrap sub-function

---
+++ R/combine_sample.R
@@ -0,0 +1,37 @@
+#' Function that combines the given time series.
+#'
+#' @param x a vector of coded vales
+#' @param y a vector of coded values
+#'
+#' @return returns a list
+#' @export
+#'
+#' @examples
+#' 
+combine_sample <- function(x, y) {
+  
+  n <- length(x)
+  newsample <- cbind(x, y)
+  newsample <- apply(newsample, 1, function(x) paste(x, collapse = ""))
+  
+  values <- sort(unique(newsample))
+  numvalues <- length(values)
+  
+  pool <- LETTERS
+  valuestab <- pool[1:numvalues]
+  names(valuestab) <- values
+  
+  csample   <- numeric(n)
+  csample[] <- NA
+  
+  if (length(pool) < numvalues) {
+    print("Too many combinations.")
+    stop
+  } else {
+    for (i in 1:numvalues){
+      csample <- ifelse(csample == values[i], pool[i], csample)
+    }
+  }
+  
+  return(list(valuestab, csample))
+}
