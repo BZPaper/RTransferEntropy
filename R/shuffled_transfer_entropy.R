d82f8662247ad827da1e2a83730182923a0efd5f
Unknown <david.zimmermann@zu.de>
2018-01-04 18:26:23 +0100

    fixed formatting

--- R/shuffled_transfer_entropy.R
+++ R/shuffled_transfer_entropy.R
@@ -1,9 +1,9 @@
-#' Function to calculate the effective transfer entropy, as given by the 
-#' difference between the transfer entropy calculated from a sample and the 
+#' Function to calculate the effective transfer entropy, as given by the
+#' difference between the transfer entropy calculated from a sample and the
 #' respective shuffled transfer entropy.
 #'
-#' @param nrep 
-#' @param shuffles 
+#' @param nrep
+#' @param shuffles
 #' @param diff if TRUE, the effective transfer entropy is calculated, otherwise
 #' only the shuffled transfer entropy is returned
 #' @param x a vector of coded values
@@ -15,41 +15,42 @@
 #' @export
 #'
 #' @examples
-#' 
-shuffled_transfer_entropy <- function(nrep = 10, 
+#'
+shuffled_transfer_entropy <- function(nrep = 10,
                                       shuffles = 6,
-                                      diff = TRUE, 
-                                      x, 
-                                      lx, 
-                                      y, 
+                                      diff = TRUE,
+                                      x,
+                                      lx,
+                                      y,
                                       ly) {
-  
+
   require(parallel)
-  
+
   n <- length(x)
   nreps <- round((nrep + 1) / shuffles)
   shuffle <- list()
-  
+
   numcores <- detectCores() - 1
   cl <- makeCluster(numcores)
-  
+
   set.seed(123)
+
   for (i in 1:shuffles) {
-    shuffle[[i]] <- replicate(nreps, 
+    shuffle[[i]] <- replicate(nreps,
                               transfer_entropy(x = x,
-                                               y = sample(y, n, replace = TRUE), 
+                                               y = sample(y, n, replace = TRUE),
                                                lx = lx, ly = ly)$transentropy)
   }
-  
+
   stopCluster(cl)
-  
+
   ste <- mean(as.numeric(as.character(unlist(shuffle))))
-  
+
   if (diff) {
     te <- transfer_entropy(x = x, y = y, lx = lx, ly = ly)$transentropy - ste
   } else {
     te <- ste
   }
-  
+
   return(te)
 }
