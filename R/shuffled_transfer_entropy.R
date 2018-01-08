c10f5a919323cefb4966972e4d8d7de0fe89ed86
Unknown <david.zimmermann@zu.de>
2018-01-08 12:49:58 +0100

    reordered parameters in shuffled TE

--- R/shuffled_transfer_entropy.R
+++ R/shuffled_transfer_entropy.R
@@ -2,28 +2,29 @@
 #' difference between the transfer entropy calculated from a sample and the
 #' respective shuffled transfer entropy.
 #'
-#' @param nreps number of replications per shuffle
-#' @param shuffles number of shuffles
-#' @param diff if TRUE, the effective transfer entropy is calculated, otherwise
-#' only the shuffled transfer entropy is returned
+
 #' @param x a vector of coded values
 #' @param lx x(k)
 #' @param y a vector of coded values
 #' @param ly y(j)
 #' @param ncores the number of cores
+#' @param nreps number of replications per shuffle
+#' @param shuffles number of shuffles
+#' @param diff if TRUE, the effective transfer entropy is calculated, otherwise
+#' only the shuffled transfer entropy is returned
 #'
 #' @return returns a numeric scalar
 #' @export
 #'
 #' @examples
 #'
-shuffled_transfer_entropy <- function(nreps = 2,
-                                      shuffles = 6,
-                                      diff = TRUE,
-                                      x,
+shuffled_transfer_entropy <- function(x,
                                       lx,
                                       y,
                                       ly,
+                                      nreps = 2,
+                                      shuffles = 6,
+                                      diff = TRUE,
                                       ncores = parallel::detectCores() - 1) {
 
   n <- length(x)
