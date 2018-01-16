e64ac577a8368e660e41fe8a0364f82fb9307454
LeeHaoyu <simon.behrendt@zu.de>
2018-01-16 20:50:30 +0100

    wrapper outline

---
+++ R/TE.R
@@ -0,0 +1,19 @@
+#' Wrapper for the implementation of Shannon and Renyi transfer entropy.
+#'
+#' @return returns a list containing the respective transfer entropy measure,
+#' the effective transfer entropy measure, standard errors, p-values and
+#' number of observations
+#' @export
+#'
+#' @examples
+#'
+TE <- function() {
+  # Treat missing values in time series
+
+
+  # Calculate transfer entropy
+
+
+  # Prepare output
+
+}
