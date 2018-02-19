03981ed3859626d8f5592d0513bb2175f5449a31
Unknown <david.zimmermann@zu.de>
2018-02-19 14:14:28 +0100

    added digits to print TEResults

--- R/TEclass.R
+++ R/TEclass.R
@@ -8,28 +8,39 @@
 #'
 #' @examples
 #' # see ?transfer_entropy()
-print.TEResult <- function(x, ...) {
+print.TEResult <- function(x, digits = 4, ...) {
   star <- function(x) {
     ifelse(is.null(x) || is.na(x), "",
            ifelse(x < 0.001, "***",
                   ifelse(x < 0.01, "**",
-                         ifelse(x < 0.05, "*", ""))))
+                         ifelse(x < 0.05, "*",
+                                ifelse(x < 0.1, ".", "")))))
   }
   fupper <- function(x) paste0(toupper(substr(x, 1, 1)), substr(x, 2, nchar(x)))
-  n <- 64
+
+  # the number of chars per reported value
+  n_digits <- max(10, digits + 2)
+  # the width (in chars) of the overall output
+  n <- 9 + (2 + n_digits) * 4 + 3 +2
+  line <- paste(rep("-", n), collapse = "")
+
+  nr_format <- sprintf("  %%%s.0%sf", n_digits, digits)
+  header_els <- paste0("%9s", paste(rep(sprintf("  %%%ss", n_digits), 4), collapse = ""), "  %s")
+  row_els <- paste0("%9s", paste(rep(nr_format, 4), collapse = ""), "  %s")
+
   str <- c(
     paste(fupper(x$entropy), "Transfer Entropy Results:"),
-    sprintf("Direction  %10s  %10s  %10s  %10s  %5s",
-            "te", "ete", "se", "p-value", "sig"),
-    paste(rep("-", n), collapse = ""),
-    sprintf("X->Y       %10.05f  %10.05f  %10.05f  %10.05f  %5s",
-            x$te_xy, x$ete_xy, x$se_xy, x$p_xy, star(x$p_xy)),
-    sprintf("Y->X       %10.05f  %10.05f  %10.05f  %10.05f  %5s",
-            x$te_yx, x$ete_yx, x$se_yx, x$p_yx, star(x$p_yx)),
-    paste(rep("-", n), collapse = ""),
+    line,
+    sprintf(header_els,
+            "Direction", "te", "ete", "se", "p-value", "sig"),
+    line,
+    sprintf(row_els, "X->Y", x$te_xy, x$ete_xy, x$se_xy, x$p_xy, star(x$p_xy)),
+    sprintf(row_els, "Y->X", x$te_yx, x$ete_yx, x$se_yx, x$p_yx, star(x$p_yx)),
+    line,
     paste0(sprintf("Number of Observations: %s", x$nobs),
            ifelse(x$entropy == "renyi", sprintf("\nQ: %s", x$q), "")),
-    paste(rep("-", n), collapse = "")
+    line,
+    "Sig. P-values:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1"
   )
   str <- paste(str, collapse = "\n")
   cat(str)
